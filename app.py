a51e22223bac173358bea4afb80fd1843872419a
SamuelVn007 <134400857+SamuelVn007@users.noreply.github.com>
2024-08-28 11:57:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,80 @@
 import os
-main():
-       mostrar_opcoes()
 
-def mostrar_opcoes():
-        print("""
-    █░░ █▀█ ░░█ ▄▀█ █▀   █▀█ █░█ ▄▀█ █▀█ ▄▀█
-    █▄▄ █▄█ █▄█ █▀█ ▄█   █▄█ █▀█ █▀█ █▀▄ █▀█""")
+Clientes = [{'nome':'Pedro Silva','instrumento musical':'clarinete','ativo':True},
+           {'nome':'Ana Julia','instrumento musical':'flauta transversal', 'ativo':False},
+           {'nome':'Hannay','instrumento musical':'ukulele', 'ativo':True}]
+   
+def exibir_titulo():
+    print("""
+█░░ █▀█ ░░█ ▄▀█ █▀   █▀█ █░█ ▄▀█ █▀█ ▄▀█
+█▄▄ █▄█ █▄█ █▀█ ▄█   █▄█ █▀█ █▀█ █▀▄ █▀█\n""")
 
 
-    print('1. Cadastra cliente')
-    print('2. Listar cliente')
-    print('3. Ativar cliente')
+def exibir_opcao():
+    print('1. Cadastrar Cliente')
+    print('2. Listar Cliente')
+    print('3. Ativar Cliente')
     print('4. Sair\n')
 
-    opcao_escolhida = int(input('escolha uma opção'))
+def escolher_opcao():
 
-def cadastrar_cliente():
-    print("Você escolheu a opção cadastrar cliente")
-def listar_cliente():
-    print ("Você escolheu a opção listar cliente")
-def ativar_cliente():
-    print ("Você escolheu a opção ativar cliente")
+    def exibir_subtitulo(texto):
+        os.system('cls')
+        print(texto)
+        print('')
 
-def finaliza_programa():
-    os.system('cls')
-    print("Finalizando programa\n")
+    def retorna_menu():
+        input(' Digite uma tecla para voltar ao menu principal ')
+        main()
+
+    def cadastra_cliente():
+        exibir_subtitulo('Cadastrar Cliente')
+       
+        Nome_Cliente = input("Digite seu Nome")
+        Clientes.append(Nome_Cliente)
+        print(f'{Nome_Cliente} foi cadastrado com sucesso\n')
+        retorna_menu()
+
+    def listar_clientes():
+        exibir_subtitulo('Listar Clientes')
+        for Cliente in Clientes:
+            nome_cliente = Cliente['nome']
+            instrumento_cliente = Cliente ['instrumento musical']
+            ativo = Cliente['ativo']
+            print(f' - {nome_cliente} | {instrumento_cliente} | {ativo}')
+        retorna_menu()
+
+    def finalizar_app():
+        os.system('cls')
+        print("Finalizando programa")
+
+    def opcao_invalida():
+        print("Opçao Invalida")
+        input("Aperte um botão para retornar")
+        main()
 
 
-if opcao_escolhida == 1:
-    cadastrar_cliente
-elif opcao_escolhida== 2:
-    listar_cliente  
-elif opcao_escolhida== 3:
-    ativar_cliente 
-else:
-   finaliza_programa
-
-   def main()
+    opcao_escolhida = int(input('Escolha uma opção:'))
     
-    if __name__ == "__name__":
-       main()
+    try:
+        if opcao_escolhida == 1:
+            cadastra_cliente()
+        elif opcao_escolhida == 2:
+            listar_clientes()
+        elif opcao_escolhida == 3:
+            print('Ativar Cliente')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+def main():
+    os.system('cls')
+    exibir_titulo()
+    exibir_opcao()
+    escolher_opcao()
+
+if __name__ == "__main__":
+    main()
